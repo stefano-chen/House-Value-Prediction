85b98deadce238142d370a9927fc8af14f3418ca
stefano-chen <stefanochen01@gmail.com>
2025-04-28 14:52:16 +0200

    preprocessing pipeline improved

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,28 +1,28 @@
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, FunctionTransformer
+from sklearn.compose import ColumnTransformer
 import pandas as pd
+import numpy as np
 from sklearn.pipeline import Pipeline
 import joblib
 
 dataset_path = "../dataset/processed/California_Houses_Processed_Train.csv"
-imputer_path = "../artifacts/preprocessing/imputer.pkl"
-scaler_path = "../artifacts/preprocessing/scaler.pkl"
 preprocessing_pipeline_path = "../artifacts/preprocessing/preprocessing_pipeline.pkl"
 
 houses_df = pd.read_csv(dataset_path)
 houses_df.drop(columns=["Median_House_Value"], inplace=True)
 
+log_transformer = FunctionTransformer(np.log, feature_names_out="one-to-one")
+
 preprocessing_pipeline = Pipeline([
-    ("impute", SimpleImputer(strategy="mean")),
+    ("impute", SimpleImputer(strategy="mean").set_output(transform="pandas")),
+    ("column transformer", ColumnTransformer([
+        ("log transformation", log_transformer, ["Population", "Households", "Median_Income"])
+    ], remainder='passthrough')),
     ("standard scaler", StandardScaler())
 ])
 
 preprocessing_pipeline.fit(houses_df)
 
-imputer = preprocessing_pipeline.named_steps["impute"]
-scaler = preprocessing_pipeline.named_steps["standard scaler"]
-
-joblib.dump(imputer, imputer_path)
-joblib.dump(scaler, scaler_path)
 joblib.dump(preprocessing_pipeline, preprocessing_pipeline_path)
 
