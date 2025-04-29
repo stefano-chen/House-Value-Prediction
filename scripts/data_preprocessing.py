2d955d0ec9cce0458b51ed4f0ff5fa56f02d6e64
stefano-chen <stefanochen01@gmail.com>
2025-04-29 14:54:49 +0200

    vscode pathing error resolved

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
     import joblib
     from pathlib import Path
 
-    dataset_path = Path("dataset/processed/California_Houses_Processed_Train.csv")
-    preprocessing_pipeline_path = Path("artifacts/preprocessing/preprocessing_pipeline.pkl")
+    dataset_path = Path("../dataset/processed/California_Houses_Processed_Train.csv")
+    preprocessing_pipeline_path = Path("../artifacts/preprocessing/preprocessing_pipeline.pkl")
     houses_df = pd.read_csv(dataset_path)
     houses_df.drop(columns=["Median_House_Value"], inplace=True)
     preprocessing_pipeline = get_preprocessing_pipeline()
